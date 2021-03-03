383d9c91ecb531b603ac535b018893141b5fc0cf
Javier Ivan Martinez Mateos <jimm0063@gmail.com>
2021-03-02 22:33:12 -0600

    First Commit

---
+++ src/main/java/com/walmart/example/api/feedback/DemoFeedbackApiApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.example.api.feedback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFeedbackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFeedbackApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/walmart/example/api/feedback/DemoFeedbackApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walmart.example.api.feedback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFeedbackApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
